e228d08dcfecdcecbe275667f1ee7e572bc526e3
Aqil326 <mdaqil123123@gmail.com>
2024-07-24 17:09:45 +0500

    data

---
+++ Assets/Plugins/3PupperStudios/Paint Materials/Examples/Assets/Scripts/MatManager.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ThreePupperStudios.PaintMaterials
+{
+    internal class MatManager : MonoBehaviour
+    {
+        [SerializeField]
+        private Renderer _walls;
+
+        public Renderer Walls => _walls;
+
+        [SerializeField]
+        private Renderer _ceiling;
+
+        public Renderer Ceiling => _ceiling;
+
+        [SerializeField]
+        private Renderer _accentWall;
+
+        public Renderer AccentWall => _accentWall;
+
+        [SerializeField]
+        private Renderer _secondaryAccent;
+
+        public Renderer SecondaryAccent => _secondaryAccent;
+
+        [SerializeField]
+        private List<MatSet> _matSets;
+
+        public List<MatSet> Materials => _matSets;
+
+        public void SetMaterial(Renderer rend, Material mat)
+        {
+            var materials = new[] {mat};
+            rend.sharedMaterials = materials;
+        }
+
+        internal List<Tuple<Texture2D, string>> GetMatNames(Category category)
+        {
+            var names = new List<Tuple<Texture2D, string>>();
+
+            var set = _matSets.Find(i => i.category == category);
+            if (set.materials != null)
+            {
+                foreach (var mat in set.materials)
+                {
+                    Texture2D texture = null;
+#if UNITY_EDITOR
+                    texture = UnityEditor.AssetPreview.GetAssetPreview(mat);
+#endif
+                    names.Add(new Tuple<Texture2D, string>(texture, mat.name));
+                }
+            }
+
+            return names;
+        }
+
+        [System.Serializable]
+        internal struct MatSet
+        {
+            public Category category;
+            public List<Material> materials;
+        }
+    }
+
+
+    internal enum Category
+    {
+        Cartagena,
+        Glossy,
+        Marble,
+        Matte,
+        Metallic,
+        Daupe,
+        Satin,
+        Spotted,
+        ThickStrokes,
+        Varnished
+    }
+}
---
+++ Assets/Plugins/3PupperStudios/Paint Materials/Examples/Assets/Scripts/UIController.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace ThreePupperStudios.PaintMaterials
+{
+    internal class UIController : MonoBehaviour
+    {
+        [SerializeField]
+        private MatManager _manager;
+
+        [SerializeField]
+        private List<RawImage> _matButtonImages = new List<RawImage>();
+
+        [SerializeField]
+        private List<Text> _matButtons = new List<Text>();
+
+        [SerializeField]
+        private Renderer _targetRenderer;
+        [SerializeField]
+        private Category _category;
+
+        private void Start()
+        {
+            SetCategory();
+        }
+
+        public void SetRenderer(Text text)
+        {
+            switch (text.text)
+            {
+                case "Walls":
+                    _targetRenderer = _manager.Walls;
+                    break;
+                case "Ceiling":
+                    _targetRenderer = _manager.Ceiling;
+                    break;
+                case "Accent 1":
+                    _targetRenderer = _manager.AccentWall;
+                    break;
+                case "Accent 2":
+                    _targetRenderer = _manager.SecondaryAccent;
+                    break;
+            }
+        }
+
+        public void SetCategory(Text text)
+        {
+            var category = text.text;
+            category = category.Replace(" ", "");
+            var canParse = Enum.TryParse(category, out Category actualCategory);
+            if (!canParse)
+            {
+                return;
+            }
+
+            _category = actualCategory;
+            SetCategory();
+        }
+
+        private void SetCategory()
+        {
+            var mats = _manager.GetMatNames(_category);
+
+            for (var i = 0; i < _matButtons.Count; i++)
+            {
+                var bttnTxt = _matButtons[i];
+                var set = mats[i];
+                if (set.Item1 != null)
+                {
+                    var bttnImage = _matButtonImages[i];
+                    bttnImage.texture = set.Item1;
+                }
+
+                bttnTxt.text = set.Item2;
+            }
+        }
+        
+        public void SetMaterial(int index)
+        {
+            var mat = _manager.Materials.Find(i => i.category == _category);
+            if (mat.materials != null)
+            {
+                if (_targetRenderer != null)
+                {
+                    _targetRenderer.sharedMaterial = mat.materials[index];
+                }
+            }
+        }
+    }
+}
